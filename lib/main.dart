8629fb69c14befc26e23f6010c2094c4f8022e98
lathifrdp <lathifrdp@gmail.com>
2023-03-16 10:40:45 +0700

    update login facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 // ignore_for_file: avoid_print
 
-import 'dart:convert';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:project_testing/bloc_rxdart_page.dart';
+import 'package:project_testing/login_facebook_page.dart';
 import 'package:project_testing/speech_text_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-String prettyPrint(Map json) {
-  JsonEncoder encoder = const JsonEncoder.withIndent('  ');
-  String pretty = encoder.convert(json);
-  print(pretty);
-  return pretty;
-}
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -26,158 +19,69 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Map<String, dynamic>? _userData;
-  AccessToken? _accessToken;
-  bool _checking = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkIfIsLogged();
-  }
-
-  Future<void> _checkIfIsLogged() async {
-    final accessToken = await FacebookAuth.instance.accessToken;
-    setState(() {
-      _checking = false;
-    });
-    if (accessToken != null) {
-      print("is Logged:::: ${prettyPrint(accessToken.toJson())}");
-      // now you can call to  FacebookAuth.instance.getUserData();
-      final userData = await FacebookAuth.instance.getUserData();
-      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
-      _accessToken = accessToken;
-      setState(() {
-        _userData = userData;
-      });
-    }
-  }
-
-  void _printCredentials() {
-    print(
-      prettyPrint(_accessToken!.toJson()),
-    );
-  }
-
-  Future<void> _login() async {
-    final LoginResult result = await FacebookAuth.instance.login(
-        // permissions: [
-        //   'public_profile',
-        //   'email',
-        // ],
-        ); // by default we request the email and the public profile
-
-    // loginBehavior is only supported for Android devices, for ios it will be ignored
-    // final result = await FacebookAuth.instance.login(
-    //   permissions: ['email', 'public_profile', 'user_birthday', 'user_friends', 'user_gender', 'user_link'],
-    //   loginBehavior: LoginBehavior
-    //       .DIALOG_ONLY, // (only android) show an authentication dialog instead of redirecting to facebook app
-    // );
-
-    if (result.status == LoginStatus.success) {
-      _accessToken = result.accessToken;
-      _printCredentials();
-      // get the user data
-      // by default we get the userId, email,name and picture
-      final userData = await FacebookAuth.instance.getUserData();
-      // final userData = await FacebookAuth.instance.getUserData(fields: "email,birthday,friends,gender,link");
-      _userData = userData;
-    } else {
-      print(result.status);
-      print(result.message);
-    }
-
-    setState(() {
-      _checking = false;
-    });
-  }
-
-  Future<void> _logOut() async {
-    await FacebookAuth.instance.logOut();
-    _accessToken = null;
-    _userData = null;
-    setState(() {});
-  }
-
-  final navigatorKey = GlobalKey<NavigatorState>();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       navigatorKey: navigatorKey,
       routes: {
-        //'/': (context) => const MyApp(),
         '/speech-text': (context) => const SpeechTextPage(),
-        '/bloc-rxdart': (context) => const BlocRxdartPage()
+        '/bloc-rxdart': (context) => const BlocRxdartPage(),
+        '/login-facebook': (context) => const LoginFacebookPage(),
       },
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Facebook Auth Example'),
+          title: const Text('Project Testing'),
         ),
-        body: _checking
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          navigatorKey.currentState?.pushNamed('/speech-text');
-                        },
-                        child: const Text(
-                          "Move to speech text",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        _userData != null
-                            ? prettyPrint(_userData!)
-                            : "NO LOGGED",
-                      ),
-                      const SizedBox(height: 20),
-                      _accessToken != null
-                          ? Text(
-                              prettyPrint(_accessToken!.toJson()),
-                            )
-                          : Container(),
-                      const SizedBox(height: 20),
-                      CupertinoButton(
-                        color: Colors.blue,
-                        onPressed: _userData != null ? _logOut : _login,
-                        child: Text(
-                          _userData != null ? "LOGOUT" : "LOGIN",
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      InkWell(
-                        onTap: () {
-                          navigatorKey.currentState?.pushNamed('/bloc-rxdart');
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16, vertical: 8),
-                          color: Colors.green,
-                          child: const Text(
-                            "Bloc RxDart",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ],
+        body: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    navigatorKey.currentState?.pushNamed("/speech-text");
+                  },
+                  child: const Text(
+                    "Move to speech text",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
                   ),
                 ),
-              ),
+                const SizedBox(
+                  height: 30,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    navigatorKey.currentState?.pushNamed('/login-facebook');
+                  },
+                  child: const Text(
+                    "Login Facebook",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                InkWell(
+                  onTap: () {
+                    navigatorKey.currentState?.pushNamed('/bloc-rxdart');
+                  },
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    color: Colors.green,
+                    child: const Text(
+                      "Bloc RxDart",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
