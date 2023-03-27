f4fb870a2d180c6c93faf73d0cf3bd7b1c030a8f
lathifrdp <lathifrdp@gmail.com>
2023-03-27 18:02:45 +0700

    fix login apple android

--- lib/login_apple_page.dart
+++ lib/login_apple_page.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: avoid_print
 
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,16 +16,24 @@ class LoginApplePage extends StatefulWidget {
 
 class _LoginApplePageState extends State<LoginApplePage> {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  Map<String, dynamic>? _credential;
 
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
+  }
+
+  String prettyPrint(Map json) {
+    JsonEncoder encoder = const JsonEncoder.withIndent('  ');
+    String pretty = encoder.convert(json);
+    print(pretty);
+    return pretty;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Example app: Sign in with Apple'),
+        title: const Text('Sign in with Apple'),
       ),
       body: Container(
         padding: const EdgeInsets.all(10),
@@ -36,16 +46,36 @@ class _LoginApplePageState extends State<LoginApplePage> {
                     AppleIDAuthorizationScopes.email,
                     AppleIDAuthorizationScopes.fullName,
                   ],
+                  webAuthenticationOptions: WebAuthenticationOptions(
+                    clientId: 'com-example-projectTesting',
+                    redirectUri: Uri.parse(
+                      'https://deserted-surf-station.glitch.me/callbacks/sign_in_with_apple',
+                    ),
+                  ),
                 );
 
-                final oauthCredential = OAuthProvider("apple.com").credential(
-                  idToken: credential.identityToken,
-                );
+                // final oauthCredential = OAuthProvider("apple.com").credential(
+                //   idToken: credential.identityToken,
+                //   accessToken: credential.authorizationCode,
+                // );
 
                 print(credential);
+                Map<String, dynamic> toJson() => {
+                      'authorizationCode': credential.authorizationCode,
+                      'email': credential.email,
+                      'familyName': credential.familyName,
+                      'givenName': credential.givenName,
+                      'identityToken': credential.identityToken,
+                      'state': credential.state,
+                      'userIdentifier': credential.userIdentifier
+                    };
+                setState(() {
+                  _credential = toJson();
+                  print(_credential);
+                });
 
-                final authResult =
-                    await _firebaseAuth.signInWithCredential(oauthCredential);
+                // final authResult =
+                //     await _firebaseAuth.signInWithCredential(oauthCredential);
 
                 final displayName =
                     '${credential.givenName} ${credential.familyName}';
@@ -53,7 +83,7 @@ class _LoginApplePageState extends State<LoginApplePage> {
 
                 print(displayName);
                 print(userEmail);
-                print(authResult);
+                // print(authResult);
               },
             ),
             const SizedBox(
